d0977966f042daed9d833678aa04ea8a910c6594
Danar Qusyairi <danar4213@gmail.com>
2026-04-16 12:53:53 +0700

    feat: initialize client project with React, Vite, Tailwind CSS, and Sidebar component

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import Sidebar from "./component/layout/Sidebar.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Sidebar />
     <App />
   </StrictMode>,
-)
+);
