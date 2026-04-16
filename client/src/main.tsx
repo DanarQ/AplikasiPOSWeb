41f02fa92a1b27a098f1078637be9b991061652d
Danar Qusyairi <danar4213@gmail.com>
2026-04-16 13:53:38 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   client/src/main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,11 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import Sidebar from "./component/layout/Sidebar.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Sidebar />
     <App />
   </StrictMode>,
 );
